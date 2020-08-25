8a2b69464f4d684d5de23359d506a43f5eac4c6a
Daniel Mills <danielmillst@gmail.com>
2020-08-25 17:12:48 -0400

    g

--- scripts/release.bat
+++ scripts/release.bat
@@ -7,7 +7,14 @@ cd lint
 
 echo ZKM Rinse Cycle
 java -Xmx4g -Xms1m -jar ZKM.jar script.zkm
-echo F|xcopy /y /f /q "out/in.jar" "out.jar"
+echo F|xcopy /y /f /q "out/in.jar" "in.jar"
+
+echo Powerwash Cycle
+java -Xmx4g -Xms1m -jar obfuscator.jar --jarIn in.jar --jarOut out.jar --config obf.json
+echo F|xcopy /y /f /q "out.jar" "in.jar"
+
+echo Rinse Cycle
+java -Xmx4g -Xms1m -jar proguard.jar @proguard.conf
 
 cd ..
 echo F|xcopy /y /s /f /q "lint/out.jar" "release/latest/Iris-%3.jar"

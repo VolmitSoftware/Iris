9a9a21350ef3af5bb3fc5154d815182d324e82b9
Daniel Mills <danielmillst@gmail.com>
2020-09-19 16:08:25 -0400

    Function sharing

--- src/main/java/com/volmit/iris/util/IrisInterpolation.java
+++ src/main/java/com/volmit/iris/util/IrisInterpolation.java
@@ -97,6 +97,16 @@ public class IrisInterpolation
 		return lerpParametric(lerpParametric(a, b, tx, v), lerpParametric(c, d, tx, v), ty, v);
 	}
 
+	public static double hermiteBezier(double p0, double p1, double p2, double p3, double mu, double tension, double bias)
+	{
+		return bezier(hermite(p0, p1, p2, p3, mu, tension, bias));
+	}
+
+	public static double hermiteParametric(double p0, double p1, double p2, double p3, double mu, double tension, double bias, double a)
+	{
+		return parametric(hermite(p0, p1, p2, p3, mu, tension, bias), a);
+	}
+
 	public static double hermite(double p0, double p1, double p2, double p3, double mu, double tension, double bias)
 	{
 		double m0, m1, mu2, mu3;
@@ -116,16 +126,6 @@ public class IrisInterpolation
 		return (a0 * p1 + a1 * m0 + a2 * m1 + a3 * p2);
 	}
 
-	public static double hermiteBezier(double p0, double p1, double p2, double p3, double mu, double tension, double bias)
-	{
-		return bezier(hermite(p0, p1, p2, p3, mu, tension, bias));
-	}
-
-	public static double hermiteParametric(double p0, double p1, double p2, double p3, double mu, double tension, double bias, double a)
-	{
-		return parametric(hermite(p0, p1, p2, p3, mu, tension, bias), a);
-	}
-
 	public static double bihermite(double p00, double p01, double p02, double p03, double p10, double p11, double p12, double p13, double p20, double p21, double p22, double p23, double p30, double p31, double p32, double p33, double mux, double muy, double tension, double bias)
 	{
 		//@builder
@@ -135,6 +135,45 @@ public class IrisInterpolation
 				hermite(p20, p21, p22, p23, muy, tension, bias), 
 				hermite(p30, p31, p32, p33, muy, tension, bias), 
 				mux, tension, bias);
+		//@done
+	}
+
+	public static double trihermite(double p000, double p001, double p002, double p003, double p010, double p011, double p012, double p013, double p020, double p021, double p022, double p023, double p030, double p031, double p032, double p033, double p100, double p101, double p102, double p103, double p110, double p111, double p112, double p113, double p120, double p121, double p122, double p123, double p130, double p131, double p132, double p133, double p200, double p201, double p202, double p203, double p210, double p211, double p212, double p213, double p220, double p221, double p222, double p223, double p230, double p231, double p232, double p233, double p300, double p301, double p302, double p303, double p310, double p311, double p312, double p313, double p320, double p321, double p322, double p323, double p330, double p331, double p332, double p333, double mux, double muy, double muz, double tension, double bias)
+	{
+		//@builder
+		return hermite(
+				
+				hermite(
+						hermite(p000, p001, p002, p003, muy, tension, bias), 
+						hermite(p010, p011, p012, p013, muy, tension, bias), 
+						hermite(p020, p021, p022, p023, muy, tension, bias), 
+						hermite(p030, p031, p032, p033, muy, tension, bias), 
+						mux, tension, bias),
+				
+				hermite(
+						hermite(p100, p101, p102, p103, muy, tension, bias), 
+						hermite(p110, p111, p112, p113, muy, tension, bias), 
+						hermite(p120, p121, p122, p123, muy, tension, bias), 
+						hermite(p130, p131, p132, p133, muy, tension, bias), 
+						mux, tension, bias),
+				
+				hermite(
+						hermite(p200, p201, p202, p203, muy, tension, bias), 
+						hermite(p210, p211, p212, p213, muy, tension, bias), 
+						hermite(p220, p221, p222, p223, muy, tension, bias), 
+						hermite(p230, p231, p232, p233, muy, tension, bias), 
+						mux, tension, bias),
+				
+				hermite(
+						hermite(p300, p301, p302, p303, muy, tension, bias), 
+						hermite(p310, p311, p312, p313, muy, tension, bias), 
+						hermite(p320, p321, p322, p323, muy, tension, bias), 
+						hermite(p330, p331, p332, p333, muy, tension, bias), 
+						mux, tension, bias),
+				muz,
+				tension, 
+				bias
+				);
 		//@done
 	}
 

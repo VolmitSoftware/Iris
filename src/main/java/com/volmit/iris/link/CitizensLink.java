9cb9719461698779918948129fe74e43ef9d5b15
Andrew <andrew@andrewcbaker.net>
2020-10-18 11:27:31 -0700

    Create Citizenslink

---
+++ src/main/java/com/volmit/iris/link/CitizensLink.java
@@ -0,0 +1,46 @@
+package com.volmit.iris.link;
+
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.Plugin;
+
+public class CitizensLink
+{
+    public CitizensLink()
+    {
+
+    }
+
+    public boolean supported()
+    {
+        return getCitizens() != null;
+    }
+
+    public Entity spawn(EntityType name, Location a)
+    {
+        if(!supported())
+        {
+            return null;
+        }
+
+        NPC npc = CitizensAPI.getNPCRegistry().createNPC(name, "");
+        npc.spawn(a);
+        return npc.getEntity();
+    }
+
+    public Plugin getCitizens()
+    {
+        Plugin p = Bukkit.getPluginManager().getPlugin("Citizens");
+
+        if(p == null)
+        {
+            return null;
+        }
+
+        return p;
+    }
+}

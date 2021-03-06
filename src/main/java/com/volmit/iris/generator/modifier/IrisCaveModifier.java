58c0b45ac88f2621416577b64076a0a0e3437be8
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-03-06 16:40:08 +0100

    Update IrisCaveModifier.java

--- src/main/java/com/volmit/iris/generator/modifier/IrisCaveModifier.java
+++ src/main/java/com/volmit/iris/generator/modifier/IrisCaveModifier.java
@@ -170,7 +170,6 @@ public class IrisCaveModifier extends EngineAssignedModifier<BlockData>
                             isAboveSea = false;
                         }
                     }
-                    ;
                 }
 
                 if(!isAboveSea){

cc3428d24bff81855f287b9935d3c159fe4fb786
Andrew Baker <andrew@andrewcbaker.net>
2021-03-15 09:16:12 -0700

    Cleanup
    Took 1 minute

--- scripts/release.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-echo Apply Script: COPY
-echo F|xcopy /y /s /f /q "%1" "%2"
-echo F|xcopy /y /s /f /q "lint/in.jar" "release/latest/Origin-%3.jar"
-echo Starting the Washing Machine
-echo F|xcopy /y /s /f /q "lint/in.jar" "release/latest/Iris-%3.jar"
-cd release
-echo F|xcopy /y /s /f /q /E "latest" "%3/"
-rmdir /Q/S latest

147d5902edf8457124e9ccbd57c7e39c27f26c4b
StrangeOne101 <strange.toby@gmail.com>
2021-07-14 15:48:05 +1200

    Attempted fix biome & region rarity
    - Attempted to fix the issue of biome & region rarity issue

--- src/main/java/com/volmit/iris/util/IRare.java
+++ src/main/java/com/volmit/iris/util/IRare.java
@@ -4,6 +4,6 @@ public interface IRare {
     int getRarity();
 
     static int get(Object v) {
-        return v instanceof IRare ? ((IRare) v).getRarity() : 1;
-    }
+		return v instanceof IRare ? Math.max(1, ((IRare) v).getRarity()) : 1;
+	}
 }

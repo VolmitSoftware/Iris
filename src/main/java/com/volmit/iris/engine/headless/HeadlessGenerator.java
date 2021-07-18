254b5c963b08936f15fcb879883abedffda02e8a
Daniel Mills <danielmillst@gmail.com>
2021-07-18 19:36:18 -0400

    Headless Generator

---
+++ src/main/java/com/volmit/iris/engine/headless/HeadlessGenerator.java
@@ -0,0 +1,38 @@
+/*
+ * Iris is a World Generator for Minecraft Bukkit Servers
+ * Copyright (c) 2021 Arcane Arts (Volmit Software)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.volmit.iris.engine.headless;
+
+import com.volmit.iris.engine.data.mca.NBTWorld;
+import com.volmit.iris.engine.framework.EngineCompositeGenerator;
+import lombok.Data;
+
+@Data
+public class HeadlessGenerator {
+    private final HeadlessWorld world;
+    private final EngineCompositeGenerator generator;
+    private final NBTWorld writer;
+
+    public HeadlessGenerator(HeadlessWorld world)
+    {
+        this.world = world;
+        generator = new EngineCompositeGenerator(world.getDimension().getLoadKey(), true);
+        generator.initialize(world.getWorld());
+        writer = new NBTWorld(world.getWorld().worldFolder());
+    }
+}

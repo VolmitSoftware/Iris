79bf3d8d4f54c1d505ed8403746877295f81fee2
Daniel Mills <danielmillst@gmail.com>
2021-07-19 10:47:20 -0400

    f

--- src/main/java/com/volmit/iris/core/tools/IrisToolbelt.java
+++ src/main/java/com/volmit/iris/core/tools/IrisToolbelt.java
@@ -133,34 +133,4 @@ public class IrisToolbelt {
     {
         IrisWorlds.evacuate(world);
     }
-
-    public static void test()
-    {
-        // Get IrisDataManager from a world
-        IrisToolbelt.access(anyWorld).getCompound().getData();
-
-        // Get Default Engine from world
-        IrisToolbelt.access(anyWorld).getCompound().getDefaultEngine();
-
-        // Get the engine at the given height
-        IrisToolbelt.access(anyWorld).getCompound().getEngineForHeight(68);
-
-        // IS THIS THING ON?
-        boolean yes = IrisToolbelt.isIrisWorld(world);
-
-        // GTFO for worlds (moves players to any other world, just not this one)
-        IrisToolbelt.evacuate(world);
-
-        IrisAccess access = IrisToolbelt.createWorld() // If you like builders...
-            .name("myWorld") // The world name
-            .dimension("terrifyinghands")
-            .seed(69133742) // The world seed
-            .headless(true)  // Headless make gen go fast
-            .pregen(PregenTask // Define a pregen job to run
-                .builder()
-                    .center(new Position2(0,0)) // REGION coords (1 region = 32x32 chunks)
-                    .radius(4)  // Radius in REGIONS. Rad of 4 means a 9x9 Region map.
-                .build())
-            .create();
-    }
 }

224d578ff84b9fdfa185316a495f0d607d052e4b
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-08-08 00:26:15 +0200

    Stream locations, add logic, return if cancel, print objects matching

--- src/main/java/com/volmit/iris/core/LocationManager.java
+++ src/main/java/com/volmit/iris/core/LocationManager.java
@@ -1,27 +1,25 @@
 package com.volmit.iris.core;
 
-import com.volmit.iris.Iris;
 import com.volmit.iris.core.tools.IrisToolbelt;
+import com.volmit.iris.engine.object.location.IrisLocation;
 import com.volmit.iris.engine.object.location.IrisLocations;
-import com.volmit.iris.util.collection.KList;
-import com.volmit.iris.util.collection.KMap;
 import com.volmit.iris.util.plugin.VolmitSender;
-import org.bukkit.Location;
 import org.bukkit.StructureType;
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-public class LocationManager implements Listener {
+import java.util.Optional;
+import java.util.stream.Stream;
 
-    // Every world has a map from StructureType to Locations
-    public static KMap<World, KMap<StructureType, KList<Location>>> locations;
+public class LocationManager implements Listener {
 
     @EventHandler
     public void on(PlayerCommandPreprocessEvent e){
 
-        getTypeFromCommand(e.getMessage());
+        if (e.isCancelled()){
+            return;
+        }
 
         if (!e.getMessage().contains("locate")){
             return;
@@ -32,44 +30,44 @@ public class LocationManager implements Listener {
         }
 
         IrisLocations locations = IrisToolbelt.access(e.getPlayer().getWorld()).getCompound().getRootDimension().getLocations();
+        VolmitSender sender = new VolmitSender(e.getPlayer());
+
+        // The command should not be passed along anymore
+        e.setCancelled(true);
 
         if (locations.isPreventLocate()){
-            e.setCancelled(true);
-            VolmitSender sender = new VolmitSender(e.getPlayer());
             sender.sendMessage("/locate is disabled in this Iris Dimension");
             return;
         }
 
+        StructureType type = getTypeFromCommand(e.getMessage());
+        final Stream<IrisLocation> locationStream = locations.getLocations().stream();
 
+        if (type == null){
+            sender.sendMessage("That structure does not exist: " + e.getMessage().split(" ")[1]);
+            sender.sendMessage("Options: " + locationStream);
+            return;
+        } else if (locationStream.noneMatch(l -> l.getType().equals(type))){
+            sender.sendMessage("The " + type.getName() + " structure exists in vanilla, but is not defined in this Dimension");
+            sender.sendMessage("Options: " + locationStream);
+            return;
+        }
 
-        // TODO: Override locations with the preset ones
+        Optional<IrisLocation> hit = locationStream.filter(l -> l.getType().equals(type)).findFirst();
+        if (hit.isPresent()){
+            sender.sendMessage("Objects matching " + type.getName() + " are " + hit.get().getObjects().toString() + ".");
+            sender.sendMessage("The locations of those objects are unknown, this functionality will be implemented later by Cyberpwn");
+        } else {
+            sender.sendMessage("No objects matching type " + type.getName() + " despite a list being defined");
+        }
     }
 
     /**
-     * Check if a location exists in a
-     * @param world world of a
-     * @param type structureType
-     * @return true if locations exist, false if not
+     * Returns the StructureType from a command if it exists, otherwise it returns null
+     * @param command The command to search
+     * @return The StructureType
      */
-    public boolean hasLocationIn(World world, StructureType type){
-        return locations.get(world).get(type).isNotEmpty();
-    }
-
-    /**
-     * Get a location in a
-     * @param world world of a
-     * @param type structureType
-     * @return a KList with locations
-     */
-    public KList<Location> getLocationIn(World world, StructureType type){
-        KList<Location> lcs = locations.get(world).get(type);
-        return lcs.isEmpty() ? null : lcs;
-    }
-
     private StructureType getTypeFromCommand(String command){
-        String structureName = command.split(" ")[1];
-        Iris.info(structureName);
-        Iris.info(command);
-        return StructureType.STRONGHOLD;
+        return StructureType.getStructureTypes().get(command.split(" ")[1]);
     }
 }

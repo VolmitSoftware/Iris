8ab79ec1ae6c290313527fac21484b23b48b89ee
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-08-14 23:25:37 +0200

    DAMNIT

--- src/main/java/com/volmit/iris/util/decree/DecreeExecutor.java
+++ src/main/java/com/volmit/iris/util/decree/DecreeExecutor.java
@@ -21,10 +21,6 @@ package com.volmit.iris.util.decree;
 import com.volmit.iris.core.tools.IrisToolbelt;
 import com.volmit.iris.engine.framework.Engine;
 import com.volmit.iris.engine.platform.PlatformChunkGenerator;
-<<<<<<< HEAD
-=======
-import com.volmit.iris.util.format.C;
->>>>>>> 486e92d... another I hate this
 import com.volmit.iris.util.plugin.VolmitSender;
 
 public interface DecreeExecutor {

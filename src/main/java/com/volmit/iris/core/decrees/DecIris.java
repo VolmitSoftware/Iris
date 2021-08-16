6d363235a579482daf016667e0ccce45de7396e5
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-08-16 23:17:51 +0200

    oops 2

--- src/main/java/com/volmit/iris/core/decrees/DecIris.java
+++ src/main/java/com/volmit/iris/core/decrees/DecIris.java
@@ -37,8 +37,6 @@ public class DecIris implements DecreeExecutor
 
     private DecPregen pregen;
 
-    private DecWhat what;
-
     @Decree(description = "Create a new world", aliases = "+")
     public void create(
             @Param(aliases = "world-name", description = "The name of the world to create", defaultValue = "IrisWorld")

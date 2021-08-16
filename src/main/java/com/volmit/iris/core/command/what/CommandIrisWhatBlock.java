fb50b4fa784c265484c9d50e7bc9d574c7e370c9
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-08-16 23:17:41 +0200

    oops

--- src/main/java/com/volmit/iris/core/command/what/CommandIrisWhatBlock.java
+++ src/main/java/com/volmit/iris/core/command/what/CommandIrisWhatBlock.java
@@ -55,7 +55,36 @@ public class CommandIrisWhatBlock extends MortarCommand {
             }
 
             if (bd != null) {
+                sender.sendMessage("Material: " + C.GREEN + bd.getMaterial().name());
+                sender.sendMessage("Full: " + C.WHITE + bd.getAsString(true));
 
+                if (B.isStorage(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Storage Block (Loot Capable)");
+                }
+
+                if (B.isLit(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Lit Block (Light Capable)");
+                }
+
+                if (B.isFoliage(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Foliage Block");
+                }
+
+                if (B.isDecorant(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Decorant Block");
+                }
+
+                if (B.isFluid(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Fluid Block");
+                }
+
+                if (B.isFoliagePlantable(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Plantable Foliage Block");
+                }
+
+                if (B.isSolid(bd)) {
+                    sender.sendMessage(C.YELLOW + "* Solid Block");
+                }
             }
         } else {
             sender.sendMessage("Players only.");

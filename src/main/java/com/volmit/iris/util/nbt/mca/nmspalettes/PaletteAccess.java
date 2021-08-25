19fa9390ff4c0337d2d4082aa51861bf15819385
cyberpwn <danielmillst@gmail.com>
2021-08-25 06:32:50 -0400

    EVEN MORE PALETTES

---
+++ src/main/java/com/volmit/iris/util/nbt/mca/nmspalettes/PaletteAccess.java
@@ -0,0 +1,33 @@
+/*
+ * Iris is a World Generator for Minecraft Bukkit Servers
+ * Copyright (c) 2021 Arcane Arts (Volmit Software)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.volmit.iris.util.nbt.mca.nmspalettes;
+
+import com.volmit.iris.util.nbt.mca.NBTWorld;
+import com.volmit.iris.util.nbt.tag.CompoundTag;
+import org.bukkit.block.data.BlockData;
+
+public interface PaletteAccess {
+    public void setBlock(int x, int y, int z, CompoundTag data);
+
+    public CompoundTag getBlock(int x, int y, int z);
+
+    public void writeToSection(CompoundTag tag);
+
+    public void readFromSection(CompoundTag tag);
+}

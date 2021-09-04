e2d07f7e787542e234fa5c4c02705d07874a7108
cyberpwn <danielmillst@gmail.com>
2021-09-04 14:00:45 -0400

    Fluid body configuration for lakes & rivers

--- src/main/java/com/volmit/iris/engine/object/IrisFluidBodies.java
+++ src/main/java/com/volmit/iris/engine/object/IrisFluidBodies.java
@@ -32,37 +32,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Snippet("carving")
+@Snippet("fluid-bodies")
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@Desc("Represents a carving configuration")
+@Desc("Represents a fluid body configuration")
 @Data
 public class IrisFluidBodies {
-    @ArrayType(type = IrisCavePlacer.class, min = 1)
-    @Desc("Define cave placers")
-    private KList<IrisCavePlacer> rivers = new KList<>();
+    @ArrayType(type = IrisRiver.class, min = 1)
+    @Desc("Define rivers")
+    private KList<IrisRiver> rivers = new KList<>();
 
-    @ArrayType(type = IrisRavinePlacer.class, min = 1)
-    @Desc("Define ravine placers")
-    private KList<IrisRavinePlacer> lakes = new KList<>();
+    @ArrayType(type = IrisLake.class, min = 1)
+    @Desc("Define lakes")
+    private KList<IrisLake> lakes = new KList<>();
 
     @BlockCoordinates
     public void doCarving(MantleWriter writer, RNG rng, Engine engine, int x, int y, int z) {
-        doCarving(writer, rng, engine, x, y, z, -1);
-    }
-
-    @BlockCoordinates
-    public void doCarving(MantleWriter writer, RNG rng, Engine engine, int x, int y, int z, int waterHint) {
         if (rivers.isNotEmpty()) {
-            for (IrisCavePlacer i : rivers) {
-                i.generateCave(writer, rng, engine, x, y, z, waterHint);
+            for (IrisRiver i : rivers) {
+                i.generate(writer, rng, engine, x, y, z);
             }
         }
 
         if (lakes.isNotEmpty()) {
-            for (IrisRavinePlacer i : lakes) {
-                i.generateRavine(writer, rng, engine, x, y, z, waterHint);
+            for (IrisLake i : lakes) {
+                i.generate(writer, rng, engine, x, y, z);
             }
         }
     }
@@ -70,11 +65,11 @@ public class IrisFluidBodies {
     public int getMaxRange(IrisData data) {
         int max = 0;
 
-        for (IrisCavePlacer i : rivers) {
+        for (IrisRiver i : rivers) {
             max = Math.max(max, i.getSize(data));
         }
 
-        for (IrisCavePlacer i : rivers) {
+        for (IrisLake i : lakes) {
             max = Math.max(max, i.getSize(data));
         }
 

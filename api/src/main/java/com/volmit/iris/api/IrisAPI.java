fc6720e090741f3aa8b3b6f1015b110866e4e49d
cyberpwn <danielmillst@gmail.com>
2021-09-09 07:00:12 -0400

    API

--- api/src/main/java/com/volmit/iris/api/IrisAPI.java
+++ api/src/main/java/com/volmit/iris/api/IrisAPI.java
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-rootProject.name = 'Iris'
-include 'api'
+package com.volmit.iris.api;
 
+public interface IrisAPI
+{
+
+}

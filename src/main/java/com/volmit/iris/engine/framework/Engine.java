2d0df240f49fff50b053c276e6ec9285c8541fab
cyberpwn <danielmillst@gmail.com>
2021-11-12 21:27:03 -0500

    Revert "f"
    This reverts commit def658ccc6e2f777f24ccb5ac212ba9c5901712d.

--- src/main/java/com/volmit/iris/engine/framework/Engine.java
+++ src/main/java/com/volmit/iris/engine/framework/Engine.java
@@ -771,7 +771,7 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
         return getBiomeOrMantle(l.getBlockX(), l.getBlockY(), l.getBlockZ());
     }
 
-    default void gotoBiome(IrisBiome biome, Player player) {
+    default void gotoBiome(IrisBiome biome, Player player, int distance, boolean random) {
         Set<String> regionKeys = getDimension()
                 .getAllRegions(this).stream()
                 .filter((i) -> i.getAllBiomes(this).contains(biome))
@@ -783,17 +783,13 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
                 && lb.matches(engine, chunk);
 
         if (!regionKeys.isEmpty()) {
-<<<<<<< HEAD
             locator.find(player, Math.abs(distance), random);
-=======
-            locator.find(player);
->>>>>>> parent of 636fddd... Improve finding by allowing minimal distance & randomization
         } else {
             player.sendMessage(C.RED + biome.getName() + " is not in any defined regions!");
         }
     }
 
-    default void gotoJigsaw(IrisJigsawStructure s, Player player) {
+    default void gotoJigsaw(IrisJigsawStructure s, Player player, int distance, boolean random) {
         if (s.getLoadKey().equals(getDimension().getStronghold())) {
             KList<Position2> p = getDimension().getStrongholds(getSeedManager().getSpawn());
 
@@ -830,7 +826,7 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
         if (getDimension().getJigsawStructures().stream()
                 .map(IrisJigsawStructurePlacement::getStructure)
                 .collect(Collectors.toSet()).contains(s.getLoadKey())) {
-            Locator.jigsawStructure(s.getLoadKey()).find(player);
+            Locator.jigsawStructure(s.getLoadKey()).find(player, distance, random);
         } else {
             Set<String> biomeKeys = getDimension().getAllBiomes(this).stream()
                     .filter((i) -> i.getJigsawStructures()
@@ -857,7 +853,7 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
             };
 
             if (!regionKeys.isEmpty()) {
-                locator.find(player);
+                locator.find(player, distance, random);
             } else {
                 player.sendMessage(C.RED + s.getLoadKey() + " is not in any defined regions, biomes or dimensions!");
             }
@@ -865,7 +861,7 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
 
     }
 
-    default void gotoObject(String s, Player player) {
+    default void gotoObject(String s, Player player, int distance, boolean random) {
         Set<String> biomeKeys = getDimension().getAllBiomes(this).stream()
                 .filter((i) -> i.getObjects().stream().anyMatch((f) -> f.getPlace().contains(s)))
                 .map(IrisRegistrant::getLoadKey)
@@ -888,19 +884,19 @@ public interface Engine extends DataProvider, Fallible, LootProvider, BlockUpdat
         };
 
         if (!regionKeys.isEmpty()) {
-            locator.find(player);
+            locator.find(player, distance, random);
         } else {
             player.sendMessage(C.RED + s + " is not in any defined regions or biomes!");
         }
     }
 
-    default void gotoRegion(IrisRegion r, Player player) {
+    default void gotoRegion(IrisRegion r, Player player, int distance, boolean random) {
         if (!getDimension().getAllRegions(this).contains(r)) {
             player.sendMessage(C.RED + r.getName() + " is not defined in the dimension!");
             return;
         }
 
-        Locator.region(r.getLoadKey()).find(player);
+        Locator.region(r.getLoadKey()).find(player, distance, random);
     }
 
     default void cleanupMantleChunk(int x, int z) {

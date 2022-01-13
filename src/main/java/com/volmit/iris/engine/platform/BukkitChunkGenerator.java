708b0d6365639aef674edbcbc06d97e3c985b647
cyberpwn <danielmillst@gmail.com>
2022-01-13 07:01:02 -0500

    2022 Sources

--- src/main/java/com/volmit/iris/engine/platform/BukkitChunkGenerator.java
+++ src/main/java/com/volmit/iris/engine/platform/BukkitChunkGenerator.java
@@ -1,6 +1,6 @@
 /*
  * Iris is a World Generator for Minecraft Bukkit Servers
- * Copyright (c) 2021 Arcane Arts (Volmit Software)
+ * Copyright (c) 2022 Arcane Arts (Volmit Software)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -170,8 +170,8 @@ public class BukkitChunkGenerator extends ChunkGenerator implements PlatformChun
                                     continue;
                                 }
 
-                                c.getBlock(xx, yy + (finalI << 4)+ world.getMinHeight(), zz)
-                                    .setBlockData(tc.getBlockData(xx, yy + (finalI << 4)+ world.getMinHeight(), zz), false);
+                                c.getBlock(xx, yy + (finalI << 4) + world.getMinHeight(), zz)
+                                    .setBlockData(tc.getBlockData(xx, yy + (finalI << 4) + world.getMinHeight(), zz), false);
                             }
                         }
                     }

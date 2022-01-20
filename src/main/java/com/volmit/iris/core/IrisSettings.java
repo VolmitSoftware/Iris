1da4ee04d30c73d0c0766f8e249fe6ccb55d56e4
cyberpwn <danielmillst@gmail.com>
2022-01-20 06:43:16 -0500

    Default headless off

--- src/main/java/com/volmit/iris/core/IrisSettings.java
+++ src/main/java/com/volmit/iris/core/IrisSettings.java
@@ -120,7 +120,7 @@ public class IrisSettings {
     @Data
     public static class IrisSettingsGenerator {
         public String defaultWorldType = "overworld";
-        public boolean headlessPregeneration = true;
+        public boolean headlessPregeneration = false;
         public int maxBiomeChildDepth = 4;
         public boolean preventLeafDecay = true;
     }

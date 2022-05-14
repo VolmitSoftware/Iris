faa7b7c7429d10c0e2d330fbe5c661192a470eca
Brian Fopiano <mypsn1550@gmail.com>
2022-05-14 12:07:27 -0700

    Object placement height now increased (need to make engine dependent)

--- src/main/java/com/volmit/iris/engine/object/IrisObjectLimit.java
+++ src/main/java/com/volmit/iris/engine/object/IrisObjectLimit.java
@@ -37,12 +37,12 @@ public class IrisObjectLimit {
     @MinNumber(0)
     @MaxNumber(1024)
     @Desc("The minimum height for placement (bottom of object)")
-    private int minimumHeight = 0;
+    private int minimumHeight = -2048; //TODO: WARNING HEIGHT
 
     @MinNumber(0)
     @MaxNumber(1024)
     @Desc("The maximum height for placement (top of object)")
-    private int maximumHeight = 255;
+    private int maximumHeight = 2048; //TODO: WARNING HEIGHT
 
     public boolean canPlace(int h, int l) {
         return h <= maximumHeight && l >= minimumHeight;

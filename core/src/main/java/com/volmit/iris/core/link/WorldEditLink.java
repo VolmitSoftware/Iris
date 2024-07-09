2296c1368b9b9bba13b645704df94fdba64dcc9a
Julian Krings <juliankrings6@gmail.com>
2024-07-09 16:19:22 +0200

    fix WorldEditLink

--- core/src/main/java/com/volmit/iris/core/link/WorldEditLink.java
+++ core/src/main/java/com/volmit/iris/core/link/WorldEditLink.java
@@ -1,12 +1,19 @@
 package com.volmit.iris.core.link;
 
+import com.volmit.iris.Iris;
+import com.volmit.iris.engine.data.cache.AtomicCache;
 import com.volmit.iris.util.data.Cuboid;
+import com.volmit.iris.util.data.KCache;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
+import java.time.Duration;
+import java.util.UUID;
+
 public class WorldEditLink {
-    private static Boolean enabled = null;
+    private static final AtomicCache<Boolean> active = new AtomicCache<>();
 
     public static Cuboid getSelection(Player p) {
         if (!hasWorldEdit())
@@ -15,29 +22,38 @@ public class WorldEditLink {
         try {
             Object instance = Class.forName("com.sk89q.worldedit.WorldEdit").getDeclaredMethod("getInstance").invoke(null);
             Object sessionManager = instance.getClass().getDeclaredMethod("getSessionManager").invoke(instance);
-            Object player = Class.forName("com.sk89q.worldedit.bukkit.BukkitAdapter").getDeclaredMethod("adapt", Player.class).invoke(null, p);
+            Class<?> bukkitAdapter = Class.forName("com.sk89q.worldedit.bukkit.BukkitAdapter");
+            Object world = bukkitAdapter.getDeclaredMethod("adapt", World.class).invoke(null, p.getWorld());
+            Object player = bukkitAdapter.getDeclaredMethod("adapt", Player.class).invoke(null, p);
             Object localSession = sessionManager.getClass().getDeclaredMethod("getIfPresent", Class.forName("com.sk89q.worldedit.session.SessionOwner")).invoke(sessionManager, player);
-            Object world = Class.forName("com.sk89q.worldedit.bukkit.BukkitAdapter").getDeclaredMethod("adapt", World.class).invoke(null, p.getWorld());
-            Object region = localSession.getClass().getDeclaredMethod("getSelection", world.getClass()).invoke(localSession, world);
+            if (localSession == null) return null;
+
+            Object region = null;
+            try {
+                region = localSession.getClass().getDeclaredMethod("getSelection", Class.forName("com.sk89q.worldedit.world.World")).invoke(localSession, world);
+            } catch (InvocationTargetException ignored) {}
+            if (region == null) return null;
+
             Object min = region.getClass().getDeclaredMethod("getMinimumPoint").invoke(region);
             Object max = region.getClass().getDeclaredMethod("getMaximumPoint").invoke(region);
             return new Cuboid(p.getWorld(),
-                    (int) min.getClass().getDeclaredMethod("getX").invoke(min),
-                    (int) min.getClass().getDeclaredMethod("getY").invoke(min),
-                    (int) min.getClass().getDeclaredMethod("getZ").invoke(min),
-                    (int) min.getClass().getDeclaredMethod("getX").invoke(max),
-                    (int) min.getClass().getDeclaredMethod("getY").invoke(max),
-                    (int) min.getClass().getDeclaredMethod("getZ").invoke(max)
+                    (int) min.getClass().getDeclaredMethod("x").invoke(min),
+                    (int) min.getClass().getDeclaredMethod("y").invoke(min),
+                    (int) min.getClass().getDeclaredMethod("z").invoke(min),
+                    (int) min.getClass().getDeclaredMethod("x").invoke(max),
+                    (int) min.getClass().getDeclaredMethod("y").invoke(max),
+                    (int) min.getClass().getDeclaredMethod("z").invoke(max)
             );
-        } catch (Throwable ignored) {
-
+        } catch (Throwable e) {
+            Iris.error("Could not get selection");
+            e.printStackTrace();
+            active.reset();
+            active.aquire(() -> false);
         }
         return null;
     }
 
     public static boolean hasWorldEdit() {
-        if (enabled == null)
-            enabled = Bukkit.getPluginManager().isPluginEnabled("WorldEdit");
-        return enabled;
+        return active.aquire(() -> Bukkit.getPluginManager().isPluginEnabled("WorldEdit"));
     }
 }

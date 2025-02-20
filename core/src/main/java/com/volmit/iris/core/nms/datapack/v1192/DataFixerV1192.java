ff2f2857842ab765416be197718b1a24a5ecce02
Julian Krings <47589149+CrazyDev05@users.noreply.github.com>
2025-02-20 23:41:19 +0100

    isolate iris world height into it's own dimension types (#1171)

--- core/src/main/java/com/volmit/iris/core/nms/datapack/v1192/DataFixerV1192.java
+++ core/src/main/java/com/volmit/iris/core/nms/datapack/v1192/DataFixerV1192.java
@@ -1,18 +1,81 @@
 package com.volmit.iris.core.nms.datapack.v1192;
 
 import com.volmit.iris.core.nms.datapack.IDataFixer;
-import com.volmit.iris.engine.object.IrisBiomeCustom;
 import com.volmit.iris.util.json.JSONObject;
+import java.util.Map;
 
 public class DataFixerV1192 implements IDataFixer {
 
-    @Override
-    public JSONObject fixCustomBiome(IrisBiomeCustom biome, JSONObject json) {
-        return json;
-    }
+    private static final Map<Dimension, String> DIMENSIONS = Map.of(
+            Dimension.OVERRWORLD, """
+            {
+              "ambient_light": 0.0,
+              "bed_works": true,
+              "coordinate_scale": 1.0,
+              "effects": "minecraft:overworld",
+              "has_ceiling": false,
+              "has_raids": true,
+              "has_skylight": true,
+              "infiniburn": "#minecraft:infiniburn_overworld",
+              "monster_spawn_block_light_limit": 0,
+              "monster_spawn_light_level": {
+                "type": "minecraft:uniform",
+                "value": {
+                  "max_inclusive": 7,
+                  "min_inclusive": 0
+                }
+              },
+              "natural": true,
+              "piglin_safe": false,
+              "respawn_anchor_works": false,
+              "ultrawarm": false
+            }""",
+            Dimension.NETHER, """
+            {
+              "ambient_light": 0.1,
+              "bed_works": false,
+              "coordinate_scale": 8.0,
+              "effects": "minecraft:the_nether",
+              "fixed_time": 18000,
+              "has_ceiling": true,
+              "has_raids": false,
+              "has_skylight": false,
+              "infiniburn": "#minecraft:infiniburn_nether",
+              "monster_spawn_block_light_limit": 15,
+              "monster_spawn_light_level": 7,
+              "natural": false,
+              "piglin_safe": true,
+              "respawn_anchor_works": true,
+              "ultrawarm": true
+            }""",
+            Dimension.THE_END, """
+            {
+              "ambient_light": 0.0,
+              "bed_works": false,
+              "coordinate_scale": 1.0,
+              "effects": "minecraft:the_end",
+              "fixed_time": 6000,
+              "has_ceiling": false,
+              "has_raids": true,
+              "has_skylight": false,
+              "infiniburn": "#minecraft:infiniburn_end",
+              "monster_spawn_block_light_limit": 0,
+              "monster_spawn_light_level": {
+                "type": "minecraft:uniform",
+                "value": {
+                  "max_inclusive": 7,
+                  "min_inclusive": 0
+                }
+              },
+              "natural": false,
+              "piglin_safe": false,
+              "respawn_anchor_works": false,
+              "ultrawarm": false
+            }"""
+    );
 
     @Override
-    public JSONObject fixDimension(JSONObject json) {
-        return json;
+    public JSONObject rawDimension(Dimension dimension) {
+        return new JSONObject(DIMENSIONS.get(dimension));
     }
 }
--- core/src/main/java/com/volmit/iris/core/tools/IrisWorldCreator.java
+++ core/src/main/java/com/volmit/iris/core/tools/IrisWorldCreator.java
@@ -21,14 +21,19 @@ package com.volmit.iris.core.tools;
 import com.volmit.iris.core.loader.IrisData;
 import com.volmit.iris.engine.object.*;
 import com.volmit.iris.engine.platform.BukkitChunkGenerator;
+import com.volmit.iris.util.reflect.WrappedField;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.generator.ChunkGenerator;
+import sun.misc.Unsafe;
 
 import java.io.File;
 
 public class IrisWorldCreator {
+    public static final WorldType IRIS;
+
     private String name;
     private boolean studio = false;
     private String dimensionName = null;
@@ -80,6 +85,7 @@ public class IrisWorldCreator {
 
 
         return new WorldCreator(name)
+                .type(IRIS)
                 .environment(findEnvironment())
                 .generateStructures(true)
                 .generator(g).seed(seed);
@@ -97,5 +103,18 @@ public class IrisWorldCreator {
     public IrisWorldCreator studio(boolean studio) {
         this.studio = studio;
         return this;
+    }
+
+    static {
+        try {
+            var unsafe = new WrappedField<Unsafe, Unsafe>(Unsafe.class, "theUnsafe").get();
+            var iris = (WorldType) unsafe.allocateInstance(WorldType.class);
+            unsafe.putIntVolatile(iris, unsafe.objectFieldOffset(Enum.class.getDeclaredField("ordinal")), 0);
+            unsafe.putObjectVolatile(iris, unsafe.objectFieldOffset(Enum.class.getDeclaredField("name")), "IRIS");
+
+            IRIS = iris;
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
     }
 }

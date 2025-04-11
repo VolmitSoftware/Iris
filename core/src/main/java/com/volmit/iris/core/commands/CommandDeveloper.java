ddcc277adc525fa7f542090d3cc01c2448cc5271
RePixelatedMC <107539181+RePixelatedMC@users.noreply.github.com>
2025-04-11 11:23:11 +0200

    Forgot to commit lol

--- core/src/main/java/com/volmit/iris/core/commands/CommandDeveloper.java
+++ core/src/main/java/com/volmit/iris/core/commands/CommandDeveloper.java
@@ -62,6 +62,7 @@ import java.util.zip.GZIPOutputStream;
 @Decree(name = "Developer", origin = DecreeOrigin.BOTH, description = "Iris World Manager", aliases = {"dev"})
 public class CommandDeveloper implements DecreeExecutor {
     private CommandTurboPregen turboPregen;
+    private CommandLazyPregen lazyPregen;
     private CommandUpdater updater;
 
     @Decree(description = "Get Loaded TectonicPlates Count", origin = DecreeOrigin.BOTH, sync = true)

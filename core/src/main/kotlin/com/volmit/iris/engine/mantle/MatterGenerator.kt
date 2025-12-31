9999f3e4293cac579a70f36dc238ae6868bddfae
Julian Krings <juliankrings6@gmail.com>
2025-12-31 13:00:45 +0100

    fix chunk artifacts

--- core/src/main/kotlin/com/volmit/iris/engine/mantle/MatterGenerator.kt
+++ core/src/main/kotlin/com/volmit/iris/engine/mantle/MatterGenerator.kt
@@ -27,8 +27,7 @@ interface MatterGenerator {
 
     @ChunkCoordinates
     fun generateMatter(x: Int, z: Int, multicore: Boolean, context: ChunkContext) {
-        if (!engine.dimension.isUseMantle || mantle.hasFlag(x, z, MantleFlag.PLANNED))
-            return
+        if (!engine.dimension.isUseMantle) return
         val multicore = multicore || IrisSettings.get().generator.isUseMulticoreMantle
 
         mantle.write(engine.mantle, x, z, radius, multicore).use { writer ->
